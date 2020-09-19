407d5a9cb0ca11c20183ad9d468a386d67e2dff4
Tristan Vandevelde <tristan.vandevelde@thomasmore.be>
2020-09-19 14:17:21 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -14,12 +14,10 @@ brew install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
-# neovim
+# CLI tools
 brew install nvim
-# tmux
 brew install tmux
-# base 16
-git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
+brew install tree
 
 
 ################
