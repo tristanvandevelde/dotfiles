78544eca8c311d00a2991af4e3e39c4a19203fb4
Tristan Vandevelde <tristan@tristanvandevelde.io>
2020-10-29 16:58:24 +0100

    Create init.lua

---
+++ dotfiles/.hammerspoon/init.lua
@@ -0,0 +1,12 @@
+--- a closure function
+function open(name)
+  return function()
+    hs.application.launchOrFocus(name)
+      if name == 'Finder' then
+        hs.appfinder.appFromName(name):activate()
+      end
+  end
+end
+
+--- quick open applications
+hs.hotkey.bind({"cmd"}, "return", open("Terminal"))
